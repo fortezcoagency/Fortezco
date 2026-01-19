6b4b4c853578f2e1bc3a57c5e6a05d276b6273d2
Fortezco Agency <fortezco.agency@gmail.com>
2026-01-19 16:02:09 +0800

    Remove final conflict marker from script.js

--- script.js
+++ script.js
@@ -347,7 +347,7 @@ document.addEventListener('DOMContentLoaded', () => {
     observer.observe(el);
   });
 });
-=======
+
 // ===================================
 // Past Projects Slider
 // ===================================
